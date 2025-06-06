442140fa51cfe4fe9209b1547dae99d856a7832c
lkar0017 <lkar0017@student.monash.edu>
2025-06-06 21:23:50 +1000

    Added responses

--- main.cpp
+++ main.cpp
@@ -10,19 +10,23 @@ int main(){
     char computer;
 
     player = getUserChoice();
-    std::cout << "Rock-Paper-Scissors Game!\n";
-    std::cout << "*************************\n";
-    std::cout << "How to Play:\n";
-    std::cout << "'R' for Rock\n";
-    std::cout << "'P' for Paper\n";
-    std::cout << "'S' for Scissors\n";
-    std::cin >> player;
+    std::cout << "You played: ";
+    showChoice(player);
 
     return 0;
 }
 
 char getUserChoice(){
-
+    char choice;
+    do{
+        std::cout << "Rock-Paper-Scissors Game!\n"; 
+        std::cout << "*************************\n";
+        std::cout << "How to Play:\n";
+        std::cout << "'R' for Rock\n";
+        std::cout << "'P' for Paper\n";
+        std::cout << "'S' for Scissors\n";
+        std::cin >> choice;
+    }while();
 }
 
 char getComputerChoice(){
@@ -30,7 +34,11 @@ char getComputerChoice(){
 }
 
 void showChoice(char choice){
-
+    switch(choice){
+        case 'r': std::cout << "Rock\n";
+        case 'p': std::cout << "Paper\n";
+        case 's': std::cout << "Scissors\n";
+    }
 }
 
 void chooseWinner(char player, char computer){
