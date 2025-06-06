58e8203297677915df3ffc562aa1c74fac477330
lkar0017 <lkar0017@student.monash.edu>
2025-06-06 21:13:37 +1000

    Created initial prompts in console

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner(char player, char computer);
+
+int main(){
+    char player;
+    char computer;
+
+    player = getUserChoice();
+    std::cout << "Rock-Paper-Scissors Game!\n";
+    std::cout << "*************************\n";
+    std::cout << "How to Play:\n";
+    std::cout << "'R' for Rock\n";
+    std::cout << "'P' for Paper\n";
+    std::cout << "'S' for Scissors\n";
+    std::cin >> player;
+
+    return 0;
+}
+
+char getUserChoice(){
+
+}
+
+char getComputerChoice(){
+
+}
+
+void showChoice(char choice){
+
+}
+
+void chooseWinner(char player, char computer){
+
+}
